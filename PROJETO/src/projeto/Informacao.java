13b192aadb7dd2fc5e2858404e7d5018d36cf3bd
Edivan <edivancarvalho2008@hotmail.com>
2019-11-21 08:07:01 -0300

    Create Informacao.java
    documento iremos por o nosso padrão de variáveis nossas classes, devemos especificar o nome da classe e sua função.

---
+++ PROJETO/src/projeto/Informacao.java
@@ -0,0 +1,2 @@
+// Bom dia neste documento iremos por o nosso padrao de variveis nossas classes, devemos especificar o nome da classe e sua função.
+// Exemplo: int countIngresso;  conta o total do ingresso ja efetuado.
