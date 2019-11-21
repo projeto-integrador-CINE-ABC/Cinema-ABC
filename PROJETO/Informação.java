907d3a8560d9a6dcff31983744615e08b5cf50ef
Edivan <edivancarvalho2008@hotmail.com>
2019-11-21 19:19:48 -0300

    Update Informação.java

--- PROJETO/Informação.java
+++ PROJETO/Informação.java
@@ -1,1 +1,7 @@
+// Bom dia neste documento iremos por o nosso padrao de variveis nossas classes, devemos especificar o nome da classe e sua função.
+// Exemplo: int countIngresso;  conta o total do ingresso ja efetuado.
+  
+1° pacote nome: JEL
+2° Class principal: Main]
 
+1° Não colocar apreviações. 
